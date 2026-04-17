a7dddce950c8399253769aba54e388a904ce0de5
mawen12 <1181963012mw@gmail.com>
2026-04-17 14:10:09 +0800

    remove unused variable

--- static/data.go
+++ static/data.go
@@ -4,34 +4,27 @@ import (
 	"embed"
 	"io/fs"
 	"net/http"
-	"os"
 )
 
 //go:embed index.html assets
 var assets embed.FS
 
 func GetFilesystem() http.FileSystem {
-	if os.Getenv("ACTUATOR_ASSETS_DEVMODE") == "1" {
-		return http.Dir("./static")
-	}
-	return mustSubFS(".")
-}
-
-func GetAssetsFilesystem() http.FileSystem {
-	if os.Getenv("ACTUATOR_ASSETS_DEVMODE") == "1" {
-		return http.Dir("./static/assets")
-	}
-	return mustSubFS("assets")
-}
-
-func GetHandler() http.Handler {
-	return http.FileServer(GetFilesystem())
-}
-
-func mustSubFS(dir string) http.FileSystem {
-	sub, err := fs.Sub(assets, dir)
+	sub, err := fs.Sub(assets, ".")
 	if err != nil {
 		panic(err)
 	}
 	return http.FS(sub)
+}
+
+func GetAssetsFilesystem() http.FileSystem {
+	sub, err := fs.Sub(assets, "assets")
+	if err != nil {
+		panic(err)
+	}
+	return http.FS(sub)
+}
+
+func GetHandler() http.Handler {
+	return http.FileServer(GetFilesystem())
 }
