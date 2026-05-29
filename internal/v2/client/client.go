a0e6f62b6182c24663237f8547fc8527f2709659
mawen12 <1181963012mw@gmail.com>
2026-05-29 22:06:05 +0800

    develop v2 client

--- internal/v2/client/client.go
+++ internal/v2/client/client.go
@@ -79,13 +79,26 @@ func (c *Client) AbilityCheck(ability string) (*UriTemplate, error) {
 	return template, nil
 }
 
-func (c *Client) Health(ctx context.Context, opts ...RequestOption) (*HealthResp, error) {
+func (c *Client) GetAbility(ability string) (string, error) {
 	link, err := c.AbilityCheck("health")
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	urlStr, err := link.Expand()
+	return link.Expand()
+}
+
+func (c *Client) GetAbilityWithParam(ability string, param map[string]interface{}) (string, error) {
+	link, err := c.AbilityCheck("health")
+	if err != nil {
+		return "", err
+	}
+
+	return link.ExpandWithParam(param)
+}
+
+func (c *Client) Health(ctx context.Context, opts ...RequestOption) (*HealthResp, error) {
+	urlStr, err := c.GetAbility("health")
 	if err != nil {
 		return nil, err
 	}
@@ -96,12 +109,7 @@ func (c *Client) Health(ctx context.Context, opts ...RequestOption) (*HealthResp
 }
 
 func (c *Client) Metrics(ctx context.Context, opts ...RequestOption) (*MetricsResp, error) {
-	link, err := c.AbilityCheck("metrics")
-	if err != nil {
-		return nil, err
-	}
-
-	urlStr, err := link.Expand()
+	urlStr, err := c.GetAbility("metrics")
 	if err != nil {
 		return nil, err
 	}
@@ -113,12 +121,7 @@ func (c *Client) Metrics(ctx context.Context, opts ...RequestOption) (*MetricsRe
 
 // tags should be provide in opts
 func (c *Client) Metric(ctx context.Context, metricName string, opts ...RequestOption) (*MetricResp, error) {
-	link, err := c.AbilityCheck("metrics-requiredMetricName")
-	if err != nil {
-		return nil, err
-	}
-
-	urlStr, err := link.ExpandWithParam(map[string]interface{}{
+	urlStr, err := c.GetAbilityWithParam("metrics-requiredMetricName", map[string]interface{}{
 		"requiredMetricName": metricName,
 	})
 	if err != nil {
@@ -131,12 +134,7 @@ func (c *Client) Metric(ctx context.Context, metricName string, opts ...RequestO
 }
 
 func (c *Client) Env(ctx context.Context, opts ...RequestOption) (*EnvResp, error) {
-	link, err := c.AbilityCheck("env")
-	if err != nil {
-		return nil, err
-	}
-
-	urlStr, err := link.Expand()
+	urlStr, err := c.GetAbility("env")
 	if err != nil {
 		return nil, err
 	}
@@ -147,12 +145,7 @@ func (c *Client) Env(ctx context.Context, opts ...RequestOption) (*EnvResp, erro
 }
 
 func (c *Client) Beans(ctx context.Context, opts ...RequestOption) (*BeansResp, error) {
-	link, err := c.AbilityCheck("env")
-	if err != nil {
-		return nil, err
-	}
-
-	urlStr, err := link.Expand()
+	urlStr, err := c.GetAbility("beans")
 	if err != nil {
 		return nil, err
 	}
@@ -163,12 +156,7 @@ func (c *Client) Beans(ctx context.Context, opts ...RequestOption) (*BeansResp, 
 }
 
 func (c *Client) Conditions(ctx context.Context, opts ...RequestOption) (*ConditionsResp, error) {
-	link, err := c.AbilityCheck("conditions")
-	if err != nil {
-		return nil, err
-	}
-
-	urlStr, err := link.Expand()
+	urlStr, err := c.GetAbility("conditions")
 	if err != nil {
 		return nil, err
 	}
@@ -179,12 +167,7 @@ func (c *Client) Conditions(ctx context.Context, opts ...RequestOption) (*Condit
 }
 
 func (c *Client) Configprops(ctx context.Context, opts ...RequestOption) (*ConditionsResp, error) {
-	link, err := c.AbilityCheck("configprops")
-	if err != nil {
-		return nil, err
-	}
-
-	urlStr, err := link.Expand()
+	urlStr, err := c.GetAbility("configprops")
 	if err != nil {
 		return nil, err
 	}
@@ -195,12 +178,7 @@ func (c *Client) Configprops(ctx context.Context, opts ...RequestOption) (*Condi
 }
 
 func (c *Client) Caches(ctx context.Context, opts ...RequestOption) (*CachesResp, error) {
-	link, err := c.AbilityCheck("caches")
-	if err != nil {
-		return nil, err
-	}
-
-	urlStr, err := link.Expand()
+	urlStr, err := c.GetAbility("caches")
 	if err != nil {
 		return nil, err
 	}
@@ -211,12 +189,7 @@ func (c *Client) Caches(ctx context.Context, opts ...RequestOption) (*CachesResp
 }
 
 func (c *Client) EvictAllCaches(ctx context.Context, opts ...RequestOption) error {
-	link, err := c.AbilityCheck("caches")
-	if err != nil {
-		return err
-	}
-
-	urlStr, err := link.Expand()
+	urlStr, err := c.GetAbility("caches")
 	if err != nil {
 		return err
 	}
@@ -226,12 +199,7 @@ func (c *Client) EvictAllCaches(ctx context.Context, opts ...RequestOption) erro
 
 // cacheManager 通过 opt 传递
 func (c *Client) EvictCache(ctx context.Context, cache string, opts ...RequestOption) error {
-	link, err := c.AbilityCheck("caches-cache")
-	if err != nil {
-		return err
-	}
-
-	urlStr, err := link.ExpandWithParam(map[string]interface{}{
+	urlStr, err := c.GetAbilityWithParam("caches-cache", map[string]interface{}{
 		"cache": cache,
 	})
 	if err != nil {
@@ -242,28 +210,18 @@ func (c *Client) EvictCache(ctx context.Context, cache string, opts ...RequestOp
 }
 
 func (c *Client) Loggers(ctx context.Context, opts ...RequestOption) (*LoggersResp, error) {
-	link, err := c.AbilityCheck("loggers")
-	if err != nil {
-		return nil, err
-	}
-
-	urlStr, err := link.Expand()
+	urlStr, err := c.GetAbility("loggers")
 	if err != nil {
 		return nil, err
 	}
 
 	var res LoggersResp
-	err = ExecuteNewRequest(ctx, http.MethodDelete, urlStr, nil, nil, append(c.Options, opts...)...)
+	err = ExecuteNewRequest(ctx, http.MethodDelete, urlStr, nil, &res, append(c.Options, opts...)...)
 	return &res, err
 }
 
 func (c *Client) SetLoggerLevel(ctx context.Context, name string, opts ...RequestOption) error {
-	link, err := c.AbilityCheck("loggers-name")
-	if err != nil {
-		return err
-	}
-
-	urlStr, err := link.ExpandWithParam(map[string]interface{}{
+	urlStr, err := c.GetAbilityWithParam("loggers-name", map[string]interface{}{
 		"name": name,
 	})
 	if err != nil {
@@ -273,32 +231,68 @@ func (c *Client) SetLoggerLevel(ctx context.Context, name string, opts ...Reques
 	return ExecuteNewRequest(ctx, http.MethodPost, urlStr, nil, nil, append(c.Options, opts...)...)
 }
 
-func (c *Client) Mappings(ctx context.Context, opts ...RequestOption) error {
-	link, err := c.AbilityCheck("mappings")
-	if err != nil {
-		return err
-	}
-
-	urlStr, err := link.Expand()
-	if err != nil {
-		return err
-	}
-
-	return ExecuteNewRequest(ctx, http.MethodPost, urlStr, nil, nil, append(c.Options, opts...)...)
-}
-
-func (c *Client) HttpExchanges(ctx context.Context, opts ...RequestOption) (*HttpExchangesResp, error) {
-	link, err := c.AbilityCheck("mappings")
+func (c *Client) Mappings(ctx context.Context, opts ...RequestOption) (*MappingsResp, error) {
+	urlStr, err := c.GetAbility("mappings")
 	if err != nil {
 		return nil, err
 	}
 
-	urlStr, err := link.Expand()
+	var res MappingsResp
+	err = ExecuteNewRequest(ctx, http.MethodGet, urlStr, nil, &res, append(c.Options, opts...)...)
+	return &res, err
+}
+
+func (c *Client) HttpExchanges(ctx context.Context, opts ...RequestOption) (*HttpExchangesResp, error) {
+	urlStr, err := c.GetAbility("httpexchanges")
 	if err != nil {
 		return nil, err
 	}
 
 	var res HttpExchangesResp
+	err = ExecuteNewRequest(ctx, http.MethodGet, urlStr, nil, &res, append(c.Options, opts...)...)
+	return &res, err
+}
+
+func (c *Client) ScheduledTasks(ctx context.Context, opts ...RequestOption) (*ScheduledTasksResp, error) {
+	urlStr, err := c.GetAbility("scheduledtasks")
+	if err != nil {
+		return nil, err
+	}
+
+	var res ScheduledTasksResp
+	err = ExecuteNewRequest(ctx, http.MethodGet, urlStr, nil, &res, append(c.Options, opts...)...)
+	return &res, err
+}
+
+func (c *Client) Togglz(ctx context.Context, opts ...RequestOption) (*TogglzResp, error) {
+	urlStr, err := c.GetAbility("togglz")
+	if err != nil {
+		return nil, err
+	}
+
+	var res TogglzResp
+	err = ExecuteNewRequest(ctx, http.MethodGet, urlStr, nil, &res, append(c.Options, opts...)...)
+	return &res, err
+}
+
+func (c *Client) UpdateTogglz(ctx context.Context, opts ...RequestOption) (*TogglzResp, error) {
+	urlStr, err := c.GetAbility("togglz")
+	if err != nil {
+		return nil, err
+	}
+
+	var res TogglzResp
+	err = ExecuteNewRequest(ctx, http.MethodPost, urlStr, nil, &res, append(c.Options, opts...)...)
+	return &res, err
+}
+
+func (c *Client) ThreadDump(ctx context.Context, opts ...RequestOption) (*ThreadResp, error) {
+	urlStr, err := c.GetAbility("threaddump")
+	if err != nil {
+		return nil, err
+	}
+
+	var res ThreadResp
 	err = ExecuteNewRequest(ctx, http.MethodGet, urlStr, nil, &res, append(c.Options, opts...)...)
 	return &res, err
 }
